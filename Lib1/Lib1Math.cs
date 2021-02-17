76147ab0a101151e3e7145339dcee3bf92b91887
Carlos Leite <CarlosLeite@criticalmanufacturing.com>
2021-02-17 15:43:57 +0000

    first commit

---
+++ Lib1/Lib1Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib1
+{
+    public static class Lib1Math
+    {
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
